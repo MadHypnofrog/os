c25b85fa050bfd621c9312792d7821872579c721
MadHypnofrog <MadHypnofrog@github.com>
2018-05-05 20:49:15 +0300

    hw5 fixed

--- web/client.c
+++ web/client.c
@@ -8,24 +8,30 @@
 #include <stdlib.h>
 int main (int argc, char** argv) {
 	char* server_name = "localhost";
-	//char* server_name = argv[0];
 	int PORT = 1337;
+	if (argc > 1) {
+		server_name = argv[1];
+	}
+	if (argc > 2) {
+		PORT = atoi(argv[2]);
+	}
+	char input[1024];
+	printf ("Enter your message: ");
+	scanf ("%s", input);
 	struct sockaddr_in server;
 	memset(&server, 0, sizeof(server));
 	server.sin_port = htons(PORT);
 	server.sin_family = AF_INET;
 	inet_pton(AF_INET, server_name, &server.sin_addr);
 	int sock;
-	if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-		fprintf (stderr, "Can't create a socket: %s\n", strerror(errno));
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		fprintf (stderr, "Can't create a sending socket: %s\n", strerror(errno));
 		return 1;
 	}
 	if (connect(sock, (struct sockaddr*) &server, sizeof(server)) < 0) {
 		fprintf (stderr, "Can't connect to server: %s\n", strerror(errno));
 		return 1;
 	}
-	char input[1024];
-	scanf ("%s", input);
 	send(sock, input, strlen(input) + 1, 0);
 	int rec = 0, length = 0, maxlength = 1024;
 	char buffer[1024];
--- web/server.c
+++ web/server.c
@@ -5,16 +5,26 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
 int main (int argc, char** argv) {
 	int PORT = 1337;
 	struct sockaddr_in server;
 	memset(&server, 0, sizeof(server));
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (argc > 1) {
+		if (inet_aton(argv[1], &server.sin_addr) == 0) {
+			fprintf(stderr, "Invalid IP-address: %s\n", strerror(errno));
+			server.sin_addr.s_addr = htonl(INADDR_ANY);
+		}
+	}
+	if (argc > 2) {
+		PORT = atoi(argv[2]);
+	}
 	server.sin_port = htons(PORT);
 	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = htonl(INADDR_ANY);
-	//server.sin_addr.s_addr = htonl(atoi(argv[0]));
 	int listen_socket;
-	if ((listen_socket = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+	if ((listen_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		fprintf (stderr, "Can't create a socket: %s\n", strerror(errno));
 		return 1;
 	}
@@ -27,32 +37,31 @@ int main (int argc, char** argv) {
 		return 1;
 	}
 	while (true) {
-		int socket;
+		int sock;
 		printf ("Waiting...\n");
-		if ((socket = accept(listen_socket, NULL, NULL)) < 0) {
+		if ((sock = accept(listen_socket, NULL, NULL)) < 0) {
 			fprintf (stderr, "Can't open a socket: %s\n", strerror(errno));
 			continue;
 		}
 		int rec = 0, length = 0, maxlength = 1024;
 		char buffer[1024];
 		char* pbuffer = buffer;
-		if ((rec = recv(socket, pbuffer, maxlength, 0)) > 0) {
-			//printf ("Received %d characters\n", rec);
+		while ((rec = recv(sock, pbuffer, maxlength, 0)) > 0) {
 			pbuffer += rec;
 			length += rec;
 			maxlength -= rec;
 			printf ("Received: %s\n", buffer);
-			if (strcmp(buffer, "exit") == 0) {
-				printf ("GG\n");
-				close(socket);
+			if (strlen(buffer) != length) {
 				break;
 			}
-			send(socket, buffer, length, 0);
-			//printf ("Sent\n");
 		}
-		//printf ("Not closed yet..\n");
-		close(socket);
-		//printf ("Closed!\n");
+		if (strcmp(buffer, "exit") == 0) {
+			printf ("Shutting down...\n");
+			close(sock);
+			break;
+		}
+		send(sock, buffer, length, 0);
+		close(sock);
 	}
 	close(listen_socket);
 	return 0;
